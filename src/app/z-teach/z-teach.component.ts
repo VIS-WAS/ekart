4da2f2901ba43e25791880adbc9436d8b83ec756
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-11 16:20:12 +0530

    ngAfterContentInit example

--- src/app/z-teach/z-teach.component.ts
+++ src/app/z-teach/z-teach.component.ts
@@ -1,5 +1,7 @@
 import {
+  AfterContentInit,
   Component,
+  ContentChild,
   DoCheck,
   ElementRef,
   Input,
@@ -14,12 +16,16 @@ import {
   templateUrl: './z-teach.component.html',
   styleUrls: ['./z-teach.component.css'],
 })
-export class ZTeachComponent implements OnChanges, OnInit, DoCheck {
+export class ZTeachComponent
+  implements OnChanges, OnInit, DoCheck, AfterContentInit
+{
   title: string = 'Demo component';
 
-  //---------//ngDocheck example//------------//
+  //---------//ngAfterContentInit example//------------//
 
   @Input() message: string;
+
+  @ContentChild('temp') paraContent: ElementRef;
 
   constructor() {
     console.log('z-teach component constructor called');
@@ -31,11 +37,18 @@ export class ZTeachComponent implements OnChanges, OnInit, DoCheck {
 
   ngOnInit() {
     console.log('ngInit hook  called');
+    console.log('In ngOnInit()', this.paraContent); //will be undefined
   }
 
   ngDoCheck() {
     console.log('ngDoCheck is called');
+    console.log('In ngDoCheck()', this.paraContent); //will be undefined
   }
 
-  //---------//ngDocheck example//------------//
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit is called');
+    console.log('In ngAfterContentInit()', this.paraContent.nativeElement);
+  }
+
+  //---------//ngAfterContentInit example//------------//
 }
