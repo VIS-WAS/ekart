0c029641d1415a66474f3052bf0fe5e44ba91754
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-11 11:09:47 +0530

    ngOnChanges and constructor example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-ekart';
+
+  //---------//ngOnchange example//------------//
+
+  inputVal: string = '';
+
+  onBtnClicked(inputEl: HTMLInputElement) {
+    this.inputVal = inputEl.value;
+  }
+  //---------//ngOnchange example//------------//
+
+  //---------//constructor example//------------//
+
+  constructor() {
+    console.log('app component constructor called');
+  }
+
+  //---------//constructor example//------------//
 }
--- src/app/z-teach/z-teach.component.ts
+++ src/app/z-teach/z-teach.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'z-teach',
   templateUrl: './z-teach.component.html',
   styleUrls: ['./z-teach.component.css'],
 })
-export class ZTeachComponent {}
+export class ZTeachComponent implements OnChanges {
+  title: string = 'Demo component';
+
+  //---------//ngOnchange example//------------//
+
+  @Input() message: string;
+
+  //---------//constructor example//------------//
+
+  constructor() {
+    console.log('z-teach component constructor called');
+    console.log(this.title);
+    console.log(this.message);
+  }
+
+  //---------//constructor example//------------//
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnchange hook  called');
+    console.log(this.message);
+    console.log(changes);
+  }
+
+  //---------//ngOnchange example//------------//
+}
