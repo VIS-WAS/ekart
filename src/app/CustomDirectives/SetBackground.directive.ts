2d5c318d2eb726f5524778f7351c07c130cb6cad
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-14 21:38:52 +0530

    property binding in Directive

--- src/app/CustomDirectives/SetBackground.directive.ts
+++ src/app/CustomDirectives/SetBackground.directive.ts
@@ -1,36 +1,28 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[setBackground]',
 })
 export class SetBackground implements OnInit {
-  // private element:ElementRef; // this can be shortcutted by using access modifier while passing property to constructor
-  // private renderer: Renderer2; //shortcut used
-  constructor(private element: ElementRef, private renderer: Renderer2) {
-    //this.element = element ;  // shortcut used
-    // this.renderer = renderer;// shortcut used
-  }
+  // -------------//Property in directive//------------------//
+
+  @Input('setBackground') backColor: string = '#36454f';
+  @Input() textColor: string = 'white';
+  @Input() fontWeight: string = '900';
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
   ngOnInit() {
-    // ----------custom attribute basic example ------------//
-
-    // this.element.nativeElement.style.backgroundColor = '#36454f';
-    // this.element.nativeElement.style.color = 'white';
-
-    // ----------custom attribute basic example ------------//
-
-    // ----------Renderer2 example ------------//
-
     this.renderer.setStyle(
       this.element.nativeElement,
       'backgroundColor',
-      '#36454f'
+      this.backColor
     );
-    this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
-    //  this.renderer.setAttribute(
-    //   this.element.nativeElement,
-    //   'title',
-    //   'this is example title'
-    // );
-    // ----------Renderer2 example ------------//
+    this.renderer.setStyle(this.element.nativeElement, 'color', this.textColor);
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'fontWeight',
+      this.fontWeight
+    );
   }
+
+  // -------------//Property in directive//------------------//
 }
