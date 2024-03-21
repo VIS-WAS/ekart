569ad632eaa2f8e16137e128c2c620b6073c1732
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-21 20:54:16 +0530

    of() and for() in observable

--- src/app/angular-observables/angular-observables.component.ts
+++ src/app/angular-observables/angular-observables.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, from, of } from 'rxjs';
 
 @Component({
   selector: 'angular-observables',
@@ -8,58 +8,51 @@ import { Observable } from 'rxjs';
 })
 export class AngularObservablesComponent {
   data: any[] = [];
+  array1 = [1, 3, 5, 7, 9, 11];
+  array2 = ['a', 'c', 'd', 'e', 'f'];
 
   //----observable
-  myObservable = new Observable((observer) => {
-    observer.next(25);
-    setTimeout(() => {
-      observer.next(76);
-    }, 1000);
-    setTimeout(() => {
-      observer.next(96);
-    }, 2000);
-    setTimeout(() => {
-      observer.next(88);
-    }, 3000);
-    setTimeout(() => {
-      observer.error(new Error('something went wrong. Pleaser try again'));
-    }, 3000);
-    setTimeout(() => {
-      observer.next(46);
-    }, 4000);
-    setTimeout(() => {
-      observer.next(6);
-    }, 5000);
-    setTimeout(() => {
-      observer.complete();
-    }, 3000);
-  });
+
+  // ------//using of()//-------//
+
+  // ---   myObservable = of(this.array1, this.array2, 1, 2, 4);
+
+  // ------//using of()//-------//
+
+  // ------//using from()//-------//
+  myObservable = from(this.array1);
+  // ------//using from()//-------//
+
+  // myObservable = new Observable((observer) => {
+  //   observer.next(25);
+  //   setTimeout(() => {
+  //     observer.next(76);
+  //   }, 1000);
+  //   setTimeout(() => {
+  //     observer.next(96);
+  //   }, 2000);
+  //   setTimeout(() => {
+  //     observer.next(88);
+  //   }, 3000);
+  //   setTimeout(() => {
+  //     observer.error(new Error('something went wrong. Pleaser try again'));
+  //   }, 3000);
+  //   setTimeout(() => {
+  //     observer.next(46);
+  //   }, 4000);
+  //   setTimeout(() => {
+  //     observer.next(6);
+  //   }, 5000);
+  //   setTimeout(() => {
+  //     observer.complete();
+  //   }, 3000);
+  // });
 
   GetAsyncData() {
-    //observer
-    //next, error, complete
-    //----------//below method is depricated to pass callback functions to subscribe------//
-    // this.myObservable.subscribe(
-    //   (val: any) => {
-    //     this.data.push(val);
-    //   },
-    //   (err) => {
-    //     alert(err.message);
-    //   },
-    //   () => {
-    //     alert('all the transfered successfully');
-    //   }
-    // );
-    //----------//below method is depricated to pass callback functions to subscribe------//
-
     this.myObservable.subscribe({
-      //-----------//below will throw error due to this.data is local to subscribe not outer call
-      // next(val: any) {
-      //   this.data.push(val);
-      // },
-
       next: (val: any) => {
         this.data.push(val);
+        console.log(this.data);
       },
 
       error(err) {
