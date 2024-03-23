0875c9af3f181ba43c7997cb2fac53f057b3a44c
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-23 18:13:18 +0530

    fromEvent observable

--- src/app/angular-observables/angular-observables.component.ts
+++ src/app/angular-observables/angular-observables.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { Observable, from, of } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Observable, Subscription, from, fromEvent, of } from 'rxjs';
 
 @Component({
   selector: 'angular-observables',
   templateUrl: './angular-observables.component.html',
   styleUrls: ['./angular-observables.component.css'],
 })
-export class AngularObservablesComponent {
+export class AngularObservablesComponent implements AfterViewInit {
+  @ViewChild('createBtn')
+  createBtn: ElementRef;
+
+  createBtnObs: Subscription;
+
+  ngAfterViewInit() {
+    this.buttonClicked();
+  }
+
+  buttonClicked() {
+    let count = 0;
+    this.createBtnObs = fromEvent(
+      this.createBtn.nativeElement,
+      'click'
+    ).subscribe((data) => {
+      console.log(data);
+      this.showItem(++count);
+    });
+  }
+
+  showItem(val) {
+    let div = document.createElement('div');
+    div.innerText = 'Item ' + val;
+    div.className = 'data-list';
+    document.getElementById('divContainer').appendChild(div);
+  }
+
   data: any[] = [];
   array1 = [1, 3, 5, 7, 9, 11];
   array2 = ['a', 'c', 'd', 'e', 'f'];
