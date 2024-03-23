9ec0cbef7944ded84e4c7055fde8037cfd1fab4d
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-23 20:10:54 +0530

    map and filter

--- src/app/angular-observables/angular-observables.component.ts
+++ src/app/angular-observables/angular-observables.component.ts
@@ -1,62 +1,76 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Observable, Subscription, from, fromEvent, of } from 'rxjs';
+import {
+  Observable,
+  Subscription,
+  filter,
+  from,
+  fromEvent,
+  map,
+  of,
+} from 'rxjs';
 
 @Component({
   selector: 'angular-observables',
   templateUrl: './angular-observables.component.html',
   styleUrls: ['./angular-observables.component.css'],
 })
-export class AngularObservablesComponent implements AfterViewInit {
-  @ViewChild('createBtn')
-  createBtn: ElementRef;
-
-  createBtnObs: Subscription;
-
-  ngAfterViewInit() {
-    this.buttonClicked();
-  }
-
-  buttonClicked() {
-    let count = 0;
-    this.createBtnObs = fromEvent(
-      this.createBtn.nativeElement,
-      'click'
-    ).subscribe((data) => {
-      console.log(data);
-      this.showItem(++count);
-    });
-  }
-
-  showItem(val) {
-    let div = document.createElement('div');
-    div.innerText = 'Item ' + val;
-    div.className = 'data-list';
-    document.getElementById('divContainer').appendChild(div);
-  }
-
+export class AngularObservablesComponent {
   data: any[] = [];
+  filterValue: any[] = [];
+  pipedValue: any[] = [];
+  shothandValue: any[] = [];
   array1 = [1, 3, 5, 7, 9, 11];
   array2 = ['a', 'c', 'd', 'e', 'f'];
 
-  promiseData = new Promise((resolve, reject) => {
-    resolve([1, 2, 3, 4, 5, 6]);
-  });
+  //-----------//shorthand method to apply map and fileter//-------------//
 
-  myObservable = from(this.promiseData);
+  myObservable = from(this.array1).pipe(
+    map((val) => {
+      return val * 3;
+    }),
+    filter((val) => {
+      return val % 5 === 0;
+    })
+  );
+  //-----------//shorthand method to apply map and fileter//-------------//
+
+  myObservableValue = from([2, 12, 5, 24, 8, 10]);
+
+  tranformDataObs = this.myObservableValue.pipe(
+    map((val) => {
+      return val * 5;
+    })
+  );
+
+  filteredObs = this.myObservableValue.pipe(
+    filter((val, i) => {
+      console.log(i);
+      return val % 4 === 0;
+    })
+  );
+
+  pipedDataobs = this.myObservableValue.pipe(
+    map((val) => {
+      return val * 3;
+    }),
+    filter((val) => {
+      return val % 5 === 0;
+    })
+  );
 
   GetAsyncData() {
-    this.myObservable.subscribe({
-      next: (val: any) => {
-        this.data.push(val);
-        console.log(this.data);
-      },
+    this.filteredObs.subscribe((val: any) => {
+      this.filterValue.push(val);
+    });
 
-      error(err) {
-        alert(err.message);
-      },
-      complete() {
-        alert('All the data transfered successfully');
-      },
+    this.tranformDataObs.subscribe((val: any) => {
+      this.data.push(val);
+    });
+    this.pipedDataobs.subscribe((val: any) => {
+      this.pipedValue.push(val);
+    });
+    this.myObservable.subscribe((val: any) => {
+      this.shothandValue.push(val);
     });
   }
 }
